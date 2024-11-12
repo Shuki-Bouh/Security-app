776998129d93c8f52ed659575f70ec88687d120e
Cyril <cyrilmaisani@yahoo.com>
2024-11-12 09:49:08 +0100

    Fix dockerfile

--- incident_management/incidents/kafkaConsumer.py
+++ incident_management/incidents/kafkaConsumer.py
@@ -6,7 +6,7 @@ from .serializers import IncidentSerializer  # Serializer pour les incidents
 # Initialiser le consommateur Kafka
 consumer = KafkaConsumer(
     'nom_topic_incident',
-    bootstrap_servers='nom_du_service_kafka:9092',
+    bootstrap_servers='kafka:9092',
     auto_offset_reset='earliest',
     enable_auto_commit=True,
     group_id='incident_consumer_group',
